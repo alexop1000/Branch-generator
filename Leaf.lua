23ec3916087c00ef98b5fa69431482b25a1557a5
Alex Op <46445843+alexop1000@users.noreply.github.com>
2021-05-02 11:40:37 +0200

    Performance Improvements

--- Leaf.lua
+++ Leaf.lua
@@ -7,12 +7,17 @@ Leaf.__index = Leaf
 function Leaf.new(width, height)
 	return setmetatable({
 		pos = Vector3.new(math.random(width), math.random(height-100)+100, 0),
-		reached = false
+		reached = false,
+		part = nil
 	},Leaf)
 end
 
 function Leaf:show()
-	local ellipse = Instance.new("Part")
+	local ellipse = self.part
+	if self.part == nil then
+		ellipse = Instance.new("Part")
+	end
+	self.part = ellipse
 	ellipse.Shape = Enum.PartType.Cylinder
 	ellipse.Size = Vector3.new(4,4,4)
 	ellipse.Position = Vector3.new(self.pos.x,self.pos.y, 0)
